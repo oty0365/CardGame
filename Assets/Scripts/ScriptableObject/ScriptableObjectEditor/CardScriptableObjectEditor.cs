c6d6f664e697ad2b02e21b675d01e02130515d13
sgoo <ppiredragonz@naver.com>
2025-03-10 13:31:59 +0900

    카드 스크렙타블오브젝트 구조

---
+++ Assets/Scripts/ScriptableObject/ScriptableObjectEditor/CardScriptableObjectEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ScriptableObject.ScriptableObjectEditor
+{
+    public enum _CardType
+    {
+        Monster,
+        Magic,
+        Passive
+    }
+    
+    [CustomEditor(typeof(CardScriptableObject))]
+    public class CardScriptableObjectEditor : Editor
+    {
+        private CardScriptableObject cardScriptableObject;
+        private Sprite sprite;
+        private GUILayoutOption[] options;
+
+        public _CardType cardType;
+        
+        public override void OnInspectorGUI()
+        {
+            cardScriptableObject = (CardScriptableObject)target;
+            
+            EditorGUILayout.LabelField("카드 설정", EditorStyles.boldLabel);
+            cardScriptableObject.CardType = (ScriptableObject._CardType)EditorGUILayout.EnumPopup(cardScriptableObject.CardType);
+            cardScriptableObject.Name = EditorGUILayout.TextField("카드 이름", cardScriptableObject.Name);
+            EditorGUILayout.LabelField("카드 설명");
+            cardScriptableObject.Description = EditorGUILayout.TextArea(cardScriptableObject.Description, GUILayout.Height(60));
+            cardScriptableObject.Sprite = (Sprite)EditorGUILayout.ObjectField("카드 이미지", cardScriptableObject.Sprite, typeof(Sprite), false);
+            EditorGUILayout.Space();
+            switch (cardScriptableObject.CardType)
+            {
+                case ScriptableObject._CardType.Monster:
+                    cardScriptableObject.Damage = EditorGUILayout.IntField("공격력", cardScriptableObject.Damage);
+                    cardScriptableObject.Health = EditorGUILayout.IntField("체력", cardScriptableObject.Health);
+                    cardScriptableObject.Cost = EditorGUILayout.IntField("가격", cardScriptableObject.Cost);
+                    break;
+                case ScriptableObject._CardType.Magic:
+                    cardScriptableObject.Cost = EditorGUILayout.IntField("가격", cardScriptableObject.Cost);
+                    break;
+                case ScriptableObject._CardType.Passive:
+                    cardScriptableObject.Cost = EditorGUILayout.IntField("가격", cardScriptableObject.Cost);
+                    break;
+            }
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(cardScriptableObject);
+            }
+        }
+    }
+}
